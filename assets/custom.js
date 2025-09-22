6d9271317ba25c9e1f4e6fcd7b14e9937f0746ef
Abraham Fernandez <abbez.net@gmail.com>
2025-09-22 09:55:22 +0800

    Added

--- assets/custom.js
+++ assets/custom.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Select all product titles you want to affect
-  document
-    .querySelectorAll(".product__title h1, .product__title h2.h1")
-    .forEach(function (title) {
-      // List the words you want to accent
-      const keywords = ["Tape", "Pull Up"];
-      let html = title.innerHTML;
-      keywords.forEach(function (word) {
-        // RegExp: match whole word, case-insensitive
-        html = html.replace(
-          new RegExp(`\\b(${word})\\b`, "gi"),
-          '<span class="product__title-accent">$1</span>'
-        );
-      });
-      title.innerHTML = html;
-    });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+
+//   document
+//     .querySelectorAll(".product__title h1, .product__title h2.h1")
+//     .forEach(function (title) {
+
+//       const keywords = ["Tape", "Pull Up"];
+//       let html = title.innerHTML;
+//       keywords.forEach(function (word) {
+
+//         html = html.replace(
+//           new RegExp(`\\b(${word})\\b`, "gi"),
+//           '<span class="product__title-accent">$1</span>'
+//         );
+//       });
+//       title.innerHTML = html;
+//     });
+// });
 
 document.addEventListener("DOMContentLoaded", function () {
   // Function to handle smooth scrolling with offset
